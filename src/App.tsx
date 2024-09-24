b323155e5dfc65d85cfcac0a53bcd5aa048fa246
jesus <jquinter46@cuc.edu.co>
2024-09-24 12:43:20 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import milogo from "../public/messi.jpg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="" />
+        <h1>world cup</h1>
+        <p>EL PRIME DEL FUTBOL</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
